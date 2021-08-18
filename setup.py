0e3a1c7c71e352799943c9e0b08e3bbad4bb6d95
Abu Kowshir Bitto <58230610+kowshir-bitto@users.noreply.github.com>
2021-08-18 20:36:13 +0600

    Add URL

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     long_description=long_description,
 
     # Here is the URL where you can find the code, in this case on GitHub.
-    url='',
+    url='https://github.com/kowshir-bitto/regresspy/tree/kowshir-bitto',
     # Here are the keywords of my library.
     keywords='gradient descent algorithm, regression, loss',
 
